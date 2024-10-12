833018211ce8048ac9b8b819efee96a8a5125f7b
Trutrucat <167733097+Trutrucat@users.noreply.github.com>
2024-10-11 17:53:56 -0700

    bootstrap added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,27 +4,36 @@ import { Link } from 'react-router-dom';
 const NavBar = ({ user, handleSignout }) => {
   return (
     <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">CHHP Inventory</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       { user ? (
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/walk-in-refrigerator">Walk-in Refrigerator</Link></li>
-            <li><Link to="/walk-in-freezer">Walk-in Freezer</Link></li>
-            <li><Link to="/store-room">Store Room</Link></li>
-            <li><Link to="/beer">Beer</Link></li>
-            <li><Link to="/dining-area">Dining Area</Link></li>
-            <li><Link to="/kitchen">Kitchen</Link></li>
-            <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
-          </ul>
-        </nav>
+        <>
+       <li className="nav-item"><Link className="nav-link"  to="/">Home</Link></li>
+       <li className="nav-item"><Link className="nav-link"  to="/walk-in-refrigerator">Walk-in Refrigerator</Link></li>
+       <li className="nav-item"><Link className="nav-link"  to="/walk-in-freezer">Walk-in Freezer</Link></li>
+       <li className="nav-item"><Link className="nav-link"  to="/store-room">Store Room</Link></li>
+       <li className="nav-item"><Link className="nav-link"  to="/beer">Beer</Link></li>
+       <li className="nav-item"><Link className="nav-link"  to="/dining-area">Dining Area</Link></li>
+       <li className="nav-item"><Link className="nav-link"  to="/kitchen">Kitchen</Link></li>
+       <li className="nav-item"><Link className="nav-link"  to="" onClick={handleSignout}>Sign Out</Link></li>
+        </>
       ) : (
-        <nav>
-          <ul>
-            <li><Link to="/signin">Sign In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
+        <>
+            <li className="nav-item"><Link className="nav-link"  to="/signin">Sign In</Link></li>
+            <li className="nav-item"><Link className="nav-link"  to="/signup">Sign Up</Link></li>
+        </>
       )}
+     </ul>   
+    </div>
+  </div>
+</nav>
+     
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'; // import BrowserRouter
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
